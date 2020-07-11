fc1c827ef8067525f3dcc8b766137ed0af40af3e
Matt Battison <matt.battison@gmail.com>
2020-07-11 18:53:16 +0100

    Print a full-width header.

--- c/tictactoe.c
+++ c/tictactoe.c
@@ -1,14 +1,56 @@
-#include<stdio.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
 
 void clearScreen() {
     printf("\x1b[2J");
     printf("\x1b[H");
 }
 
+
+void getWindowSize(int *rows, int *cols) {
+    struct winsize ws;
+
+    ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws);
+
+    *cols = ws.ws_col;
+    *rows = ws.ws_row;
+}
+
+
+void printHeader(int cols) {
+
+    for (int i = 0; i < cols; i++) {
+        printf("=");
+    }
+    printf("\n");
+    for (int i = 0; i < (cols/2 - 7); i++) {
+        printf("=");
+    }
+    printf(" TIC TAC TOE ");
+    for (int i = 0; i < (cols/2 - 6); i++) {
+        printf("=");
+    }
+    if (cols % 2 != 0) {
+        printf("=");
+    }
+    printf("\n");
+    for (int i = 0; i < cols; i++) {
+        printf("=");
+    }
+    printf("\n");
+
+}
+
+
 int main() {
     clearScreen();
-    printf("===========\n");
-    printf("TIC TAC TOE\n");
-    printf("===========\n");
+
+    int rows;
+    int cols;
+    getWindowSize(&rows, &cols);
+
+    printHeader(cols);
+
     return 0;
 }
