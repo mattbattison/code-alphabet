7eac82234e182897335a772f9194a4680dccb9cf
Matt Battison <matt.battison@gmail.com>
2020-07-11 23:54:07 +0100

    Change the win checks to if...else if instead of all ifs.
    It is only possible to win one way at a time.

--- c/tictactoe.c
+++ c/tictactoe.c
@@ -120,29 +120,29 @@ void checkForWinner(char* board) {
     if ((board[0] == board[1]) && (board[1] == board[2])) {
         winner = board[0];
     }
-    if ((board[3] == board[4]) && (board[4] == board[5])) {
+    else if ((board[3] == board[4]) && (board[4] == board[5])) {
         winner = board[3];
     }
-    if ((board[6] == board[7]) && (board[7] == board[8])) {
+    else if ((board[6] == board[7]) && (board[7] == board[8])) {
         winner = board[6];
     }
 
     // columns
-    if ((board[0] == board[3]) && (board[3] == board[6])) {
+    else if ((board[0] == board[3]) && (board[3] == board[6])) {
         winner = board[0];
     }
-    if ((board[1] == board[4]) && (board[4] == board[7])) {
+    else if ((board[1] == board[4]) && (board[4] == board[7])) {
         winner = board[1];
     }
-    if ((board[2] == board[5]) && (board[5] == board[8])) {
+    else if ((board[2] == board[5]) && (board[5] == board[8])) {
         winner = board[2];
     }
 
     // diagonals
-    if ((board[0] == board[4]) && (board[4] == board[8])) {
+    else if ((board[0] == board[4]) && (board[4] == board[8])) {
         winner = board[0];
     }
-    if ((board[2] == board[4]) && (board[4] == board[6])) {
+    else if ((board[2] == board[4]) && (board[4] == board[6])) {
         winner = board[2];
     }
 
